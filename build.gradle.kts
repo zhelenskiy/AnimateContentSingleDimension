0c6423bf13553617eb91b4f493667f1171ad3afc
Evgeniy.Zhelenskiy <zhelenskiy2000@yandex.ru>
2024-02-21 16:18:32 +0100

    Fix GitHub workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,6 @@ plugins {
     `maven-publish`
 }
 
-val GITHUB_USER: String by project
-val GITHUB_TOKEN: String by project
-
 group = "com.zhelenskiy"
 version = "0.0.1-SNAPSHOT"
 
@@ -13,8 +10,8 @@ publishing {
         maven {
             url = uri("https://maven.pkg.github.com/zhelenskiy/AnimateContentSingleDimension")
             credentials {
-                username = GITHUB_USER
-                password = GITHUB_TOKEN
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
