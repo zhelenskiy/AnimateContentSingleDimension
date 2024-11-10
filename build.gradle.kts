698717a809116dacc64e3468c942f9cbfa5bf2d8
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2024-11-10 06:35:31 +0100

    Update Kotlin, Compose and Amper

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "com.zhelenskiy"
-version = "1.0.3"
+version = "1.0.4"
 
 publishing {
     repositories {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,16 @@
 rootProject.name = "animate-content-single-dimension"
 
 pluginManagement {
-  repositories {
-    mavenCentral()
-    google()
-    gradlePluginPortal()
-    maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
-    maven("https://www.jetbrains.com/intellij-repository/releases")
-    maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
-  }
+    repositories {
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+        maven("https://packages.jetbrains.team/maven/p/amper/amper")
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
+    }
 }
 
 plugins {
-  id("org.jetbrains.amper.settings.plugin").version("0.2.3-dev-473")
+    id("org.jetbrains.amper.settings.plugin").version("0.5.0")
 }
